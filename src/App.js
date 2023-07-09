11cf1a4a94f5de349d8c6be4bc1b88f44b3942a8
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-07-09 19:27:46 +0530

    component-navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h4>This is Priyanshi Agrawal.</h4>
     </div>
   );
