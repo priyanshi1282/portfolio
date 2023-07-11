c129375bb47d447ab8666765aad4bc29c1d04722
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-07-11 23:40:54 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Home from './pages/home';
+import About from './pages/about';
+
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
       <h4>This is Priyanshi Agrawal.</h4>
     </div>
   );
