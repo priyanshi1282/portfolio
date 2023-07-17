5c0395957afb3336cd29c303f292a83c28574afe
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-07-17 23:28:49 +0530

    navbar structure done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
-import Home from './pages/home';
-import About from './pages/about';
+// import Home from './pages/home';
+// import About from './pages/about';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      {/* <Home />
       <About />
-      <h4>This is Priyanshi Agrawal.</h4>
+      <h4>This is Priyanshi Agrawal.</h4> */}
     </div>
   );
 }
