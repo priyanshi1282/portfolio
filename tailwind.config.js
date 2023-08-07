6df19724682add74bb145877795a49a8f015aea1
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-08-07 18:33:12 +0530

    color variables added in config file of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          primary : '#b7e43a',
+          primaryGreen200 : "#95A058",
+          secondaryBlack : "#222222",
+          secondaryGray100 : "#CECECE",
+          secondaryGray200 : "#3C3C3C"
+
+      },
+    },
   },
   plugins: [],
 }
