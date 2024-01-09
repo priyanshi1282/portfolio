b98922ce6d90a1a3bcce4c4df3aec493be4c1715
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-09 23:59:47 +0530

    next app init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors:{
-          primary : '#b7e43a',
-          primaryGreen200 : "#95A058",
-          secondaryBlack : "#222222",
-          secondaryGray100 : "#CECECE",
-          secondaryGray200 : "#3C3C3C"
-
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
 }
-
