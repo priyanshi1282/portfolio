b56c32c25acbbc3b8490aec8e472ee2cf84ae9a3
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-13 18:35:02 +0530

    react icons library installed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 // works as app.js in create-react-app
 import Image from 'next/image'
 import Link from 'next/link'
-
+import Navbar from '@/components/Navbar'
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Link href='/experience'>
         experience
       </Link>
---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div className="nav-header">
+                <div className="nav-brand">
+                    <h1>pa.</h1>
+                </div>
+                <div className="nav-toggle">
+                    <button></button>
+                </div>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
