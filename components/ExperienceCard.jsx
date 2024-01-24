e05f5756508f9cc1d6d8762db7928347ad717a1b
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-24 13:07:20 +0530

    desktop view exp card styling done

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -15,21 +15,26 @@ const ExperienceCard = (props) => {
     } = props.item;
 
   return (
-    <div>
-        <div>
-            <p>{date}</p>
-            <p>{location}</p>
+    <div className=" flex md:flex-row flex-col-reverse md:gap-8 gap-4">
+        {/* left side - date/location */}
+        <div className="md:w-3/12">
+            <p className="text-base">{date}</p>
+            <p className="text-xs">{location}</p>
         </div>
-        <div>
-            <div>
+
+        {/* right side -  all info */}
+        <div className="md:w-[calc(100%-25%)]">
+            <div className="md:flex items-center gap-4">
+                <div className="w-[50px] h-[50px] rounded-full bg-white shadow-sm flex items-center p-2">
                 <Image width={100} height={100} src={logo} />
+                </div>
                 <div>
-                    <p>{company} <Link href={url}><FaExternalLinkAlt /></Link></p>
-                    <p>{role}</p>
+                    <p className="items-center gap-3 font-medium">{company} <Link href={url} className="inline-flex"><FaExternalLinkAlt /></Link></p>
+                    <p className="text-sm">{role}</p>
                 </div>
             </div>
-            <p>{description}</p>
-            <Image width={150} height={150} src={skills} />
+            <p className="mt-3">{description}</p>
+            <Image width={150} height={150} src={skills} className="mt-3" />
         </div>
     </div>
   )
