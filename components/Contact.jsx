79831e2ff6793dfb272630165d39ff399cbd8ee1
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-28 20:04:16 +0530

    contact section made responsive

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,14 +6,14 @@ const Contact = () => {
   return (
     <section>
         <Heading title="Contact Me!" />
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center flex-col-reverse md:flex-row gap-10">
             <div className="overflow-hidden rounded-full">
-                <Image src="https://ik.imagekit.io/priyanshi/Portfolio/avatar.jpeg?updatedAt=1706277685939" width={500} height={500} className="rounded-full w-[320px] hover hover:grayscale-[50%] " />
+                <Image src="https://ik.imagekit.io/priyanshi/Portfolio/avatar.jpeg?updatedAt=1706277685939" width={500} height={500} className="rounded-full w-[250px] md:w-[320px] hover hover:grayscale-[50%] " />
             </div>
             <div>
                 <form className="flex flex-col gap-4">
                     {/* row1 */}
-                    <div className="flex gap-4 flex-wrap">
+                    <div className="flex gap-4 md:flex-row flex-col">
                         {/* item */}
                         <div className="flex flex-col gap-2 bg-white rounded-md px-4 py-2 shadow">
                             <label className="text-sm font-semibold">Name<span className="text-red-500">*</span></label>
