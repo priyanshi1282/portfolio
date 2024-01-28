61c6ae89edafd2e49d335188d345372b8a170c5d
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-28 19:59:54 +0530

    contact form updated

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
                 <Image src="https://ik.imagekit.io/priyanshi/Portfolio/avatar.jpeg?updatedAt=1706277685939" width={500} height={500} className="rounded-full w-[320px] hover hover:grayscale-[50%] " />
             </div>
             <div>
-                <form>
+                <form className="flex flex-col gap-4">
                     {/* row1 */}
                     <div className="flex gap-4 flex-wrap">
                         {/* item */}
@@ -27,13 +27,13 @@ const Contact = () => {
                     </div>
 
                     {/* row2 */}
-                    <div className="flex flex-col gap-2 bg-white rounded-md px-4 py-2 shadow mt-4">
+                    <div className="flex flex-col gap-2 bg-white rounded-md px-4 py-2 shadow">
                             <label className="text-sm font-semibold">Message<span className="text-red-500">*</span></label>
                             <textarea cols="30" rows="3" required placeholder="Enter your message" className="outline-none "></textarea>
                     </div>
                     
                     {/* row3 */}
-                    <button className='btn btn-bg hover mt-4'>Send <IoSendSharp /></button>
+                    <button className='btn btn-bg hover w-fit'>Send <IoSendSharp /></button>
                 </form>
             </div>
         </div>
