b9e00a181dde67433d34b48aede1066e3356062d
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-02-04 22:36:40 +0530

    skills updated

--- components/Terminal.jsx
+++ components/Terminal.jsx
@@ -77,7 +77,7 @@ const Terminal = () => {
                         {/* ans */}
                         <div className="ans">
                             <p>
-                                ["Java", "React JS", "Tailwind/Bootstrap", "Git", "MySQL", "Designing"]
+                                ["Java", "React JS", "Sass", "Tailwind/Bootstrap", "Git", "MySQL", "Designing"]
                             </p>
                         </div>
                     </div>
